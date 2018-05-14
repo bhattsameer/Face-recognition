93601237644e86c5989b249a919a64551771eacd
Sameer Bhatt (debugger) <bhattsameer5@gmail.com>
2018-05-14 18:18:33 +0530

    Add files via upload

---
+++ datasetCreator.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cam = cv2.VideoCapture(0)
+
+id = raw_input('Enter user id: ')
+#name = raw_input('Enter Name: ')
+sampleNum=0
+while(True):
+    ret,img = cam.read() #colored iamge here
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # convert color image to plan color
+    faces = faceDetect.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        sampleNum=sampleNum+1
+        cv2.imwrite("dataSet/User."+str(id)+"."+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.waitKey(100)
+    cv2.imshow("Face",img);
+    cv2.waitKey(1)
+    if(sampleNum>20):
+        break
+cam.release()
+cv2.destroyAllWindows()
+    
